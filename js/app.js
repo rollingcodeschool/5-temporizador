efa733ae92379c4d021320d55fd5adcf6ee10000
Emilse Arias <emilsearias@gmail.com>
2022-07-22 11:35:39 -0300

    se agrego la funcionalidad del temporizador

--- js/app.js
+++ js/app.js
@@ -1,11 +1,72 @@
-let minutos = document.querySelector('#minutos').value
-let segundos = document.querySelector('#segundos').value
+let tiempo = document.querySelector("#tiempo");
+let btnIniciar = document.querySelector("#btnIniciar");
+let btnDetener = document.querySelector("#btnDetener");
+let intervalo =0;
 
-let formTemp = document.querySelector('#formTemp')
-let tiempo = document.querySelector('#tiempo')
-formTemp.addEventListener('submit', iniciar)
+let minutos = 0,
+  segundos = 0;
+console.log(tiempo);
 
-function iniciar(e){
-    e.preventDefault();
-    tiempo.innerHTML=`${minutos}: ${segundos}`
+function establecerTiempo(temp) {
+  btnIniciar.className = "btn btn-success";
+  switch (temp) {
+    case 15:
+    case 30:
+      segundos = temp;
+      tiempo.innerHTML = `00: ${segundos}`;
+      break;
+    case 1:
+    case 2:
+      minutos = temp;
+      tiempo.innerHTML = `0${minutos}: 00`;
+      break;
+    default:
+      console.log("tiempo no establecido");
+  }
+}
+
+function iniciar() {
+  btnIniciar.className = "btn btn-success disabled";
+  btnDetener.className = "btn btn-danger";
+ 
+
+  if (minutos > 0) {
+    minutos--;
+    segundos = 60;
+    intervalo = setInterval(() => {
+      segundos--;
+      tiempo.innerHTML =
+        segundos < 10
+          ? `0${minutos}: 0${segundos}`
+          : `0${minutos}: ${segundos}`;
+      console.log(segundos);
+      if (minutos === 0 && segundos === 0) {clearInterval(intervalo);  btnDetener.className = "btn btn-danger disabled";}
+      if (segundos === 0 && minutos > 0) {
+        minutos--;
+      }
+    }, 1000);
+    return;
+  }
+  if (segundos > 0 && minutos === 0) {
+    console.log("en el de segundos");
+    intervalo = setInterval(() => {
+      segundos--;
+      tiempo.innerHTML = segundos < 10 ? `00: 0${segundos}` : `00: ${segundos}`;
+      console.log(segundos);
+      if (segundos === 0) {
+        clearInterval(intervalo);
+        btnDetener.className = "btn btn-danger disabled";
+      }
+    }, 1000);
+  }
+}
+
+function detener(){
+    tiempo.innerHTML =`00: 00`;
+    minutos=0, segundos= 0;
+    clearInterval(intervalo);
+    console.log(minutos);
+    btnIniciar.className = "btn btn-success disabled";
+  btnDetener.className = "btn btn-danger disabled" ;
+ 
 }
